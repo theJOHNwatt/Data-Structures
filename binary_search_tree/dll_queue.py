544557acd94f62a5528c1cc5b1e3d8fadf69a616
thejohnwatt <j.watt10@gmail.com>
2020-02-27 12:50:11 -0800

    MVP day 3-4

---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.storage.tail is None:
+            return None
+        self.size -=1
+        return self.storage.remove_from_tail()
+
+    def len(self):
+        return self.size
